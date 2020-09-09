0e8096e18afe88eae29a11e0a65069f728926817
bryansoebagijo <bryan.s0512@gmail.com>
2020-09-09 11:23:52 +0700

    asset prefix 2

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
     return config
   },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: isProd? process.env.NEXT_PUBLIC_BASE_PATH : '',
 };
 
 // next.config.js
