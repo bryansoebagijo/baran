5d121ae1d48fd7e9b49f2e17c3ba9fbe5c506995
bryansoebagijo <bryan.s0512@gmail.com>
2020-09-29 13:28:20 +0700

    overwrite productlist with real data ver 7

--- pages/products.js
+++ pages/products.js
@@ -226,7 +226,7 @@ function products() {
             console.log(tempFetchData);
             outputfilter = productlist.filter((product)=>{
                 console.log(product.connection);
-                return product.connection === '1'
+                return product.connection == 1
             })
             console.log(outputfilter);
             setDataFilter(outputfilter)
@@ -240,7 +240,7 @@ function products() {
             console.log(tempFetchData);
             outputfilter = productlist.filter((product)=>{
                 console.log(product.connection);
-                return product.connection === '0'
+                return product.connection == 0
             })
             console.log(outputfilter);
             setDataFilter(outputfilter)
