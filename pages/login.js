b027f6e257fd5e63c258e5c5969eec8a88797355
bryansoebagijo <bryan.s0512@gmail.com>
2020-10-14 11:03:30 +0700

    try fixing PWA

--- pages/login.js
+++ pages/login.js
@@ -106,7 +106,7 @@ function login() {
 
     return (
             <div className="login">
-                <Head></Head>
+                <Head props={{description:"login"}}></Head>
                 <nav className="navbar">
                     <img src={baran_white} alt="Baran logo" className="navbar-brand"></img>
                     <i className="fa fa-map icon-pajangan" aria-hidden="true"></i>
