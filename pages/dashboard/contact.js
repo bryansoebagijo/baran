d0f8b21db26418f1eec06460aa81d2f92e62bc08
bryansoebagijo <bryan.s0512@gmail.com>
2020-10-15 13:03:03 +0700

    error login

--- pages/dashboard/contact.js
+++ pages/dashboard/contact.js
@@ -175,7 +175,6 @@ function contact() {
         return (
             <div className='container-fluid' style={{ "height": "30vh" }}>
                 <div className='d-flex flex-column justify-content-center align-items-center mt-5' style={{ "width": "100%", "height": "100%", "fontFamily": "Raleway" }}>
-                    <h2 style={{ "color": "#fafafa" }}>Loading....</h2>
                     <Loader type='ThreeDots' color="#00BFFF" height={60} width={60} />
                 </div>
             </div>
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -745,7 +745,6 @@ function dashboard() {
         return (
             <div className='container-fluid' style={{"height":"30vh"}}>
                 <div className='d-flex flex-column justify-content-center align-items-center mt-5' style={{ "width": "100%", "height": "100%", "fontFamily":"Raleway"}}>
-                    <h3 style={{"color":"#fafafa"}}>Loading....</h3>
                     <Loader type='ThreeDots' color="#00BFFF" height={60} width={60} />
                 </div>
             </div>
